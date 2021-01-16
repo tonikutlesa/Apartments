63bd34237f45f38e3d4ae759bccf09d11b1ab4cb
Kutlex <toni.kutlesa@gmail.com>
2021-01-16 17:16:13 +0100

    adding contact bar

---
+++ src/components/contactBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const ContactBar = () => (
+    <main className={styles.container}>
+        <span>VillaKutlesa@gmail.com</span>
+        <span>091/472-8134</span>
+    </main>
+)
+
+export default ContactBar
